ff15507251121a204bb600cae80cfbf24b1797a9
Shivam Kumar <shivam123425@gmail.com>
2020-02-08 23:10:15 +0530

    Basic nextjs application boilerplate

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar";
+
+const About = () => (
+  <div>
+    <Navbar />
+    <h1>About BitzPice</h1>
+    <p>Application to view bitcoin prices.</p>
+  </div>
+);
+
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar";
+
+const Index = () => (
+  <div>
+    <Navbar />
+    <h1>Welcome to Bitz Price</h1>
+  </div>
+);
+
+export default Index;
+<h1>Welcome to About Page</h1>;
